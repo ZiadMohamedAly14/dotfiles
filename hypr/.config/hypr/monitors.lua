e9251ed72c1a3c31b1130409014cfc0d6908ea42
ZiadMohamedAly14 <iziadmohamedaly14@gmail.com>
2026-09-09 07:55:08 +0300

    dec sens and pin main monitor

--- hypr/.config/hypr/monitors.lua
+++ hypr/.config/hypr/monitors.lua
@@ -8,6 +8,8 @@ local omarchy_monitor_scale = "auto"
 hl.env("GDK_SCALE", tostring(omarchy_gdk_scale))
 hl.monitor({ output = "", mode = "preferred", position = "auto", scale = omarchy_monitor_scale })
 
+hl.monitor({ output = "DP-1", mode = "1920x1080@165", position = "auto", scale = 1 })
+
 -- Pin a specific monitor.
 -- hl.monitor({ output = "DP-2", mode = "2560x1440@144", position = "0x0", scale = 1 })
 
