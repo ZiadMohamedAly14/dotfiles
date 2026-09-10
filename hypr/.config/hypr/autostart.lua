935ed41bd36a2647e23698a50dea848acb07afb7
ZiadMohamedAly14 <iziadmohamedaly14@gmail.com>
2026-09-10 09:21:20 +0300

    nvidia vibrance with 2sec delay

--- hypr/.config/hypr/autostart.lua
+++ hypr/.config/hypr/autostart.lua
@@ -3,6 +3,7 @@
 
 -- o.launch_on_start("nextcloud --background")
 -- o.launch_on_start("syncthing serve --no-browser")
+o.launch_on_start("sleep 2 && nvibrant 614")
 
 -- For things that should run on system events rather than session start
 -- (post-boot, post-update, theme-set, battery-low), use an Omarchy hook
